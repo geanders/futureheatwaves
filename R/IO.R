a6359bd089c54e80d9c9d48abd1b5a1f2a5c576a
Brooke Anderson <geanderson@jhsph.edu>
2016-02-02 20:18:35 -0700

    Close #56.

--- R/IO.R
+++ R/IO.R
@@ -79,7 +79,7 @@ writeThresholds <- function(modelName, threshOut, thresholds, global, custom){
 
         # Create the directory that the file will be written to
         dir.create(paste(writePath, modelName, sep = ""),
-                   showWarnings = FALSE, recursive = TRUE, mode = "0777")
+                   showWarnings = FALSE, recursive = TRUE)
 
         # Write the file
         write.csv(writeThis, file = paste(writePath, modelName,
@@ -130,8 +130,7 @@ createEnsembleWriter <- function(modelName, global, custom){
 
                 # Create the directory that the file will be written to
                 dir.create(paste(writePath, modelName, sep = ""),
-                           showWarnings = FALSE, recursive = TRUE,
-                           mode = "0777")
+                           showWarnings = FALSE, recursive = TRUE)
 
                 # Write the file
                 write.csv(hwFrame,

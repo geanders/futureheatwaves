b8b8e2f9efdf9cf39db6d1ef7e16b58d0eed9676
colinae <colinae@rams.colostate.edu>
2016-02-02 21:17:20 -0700

    Updated C++ functions

--- R/IDHeatwaves.R
+++ R/IDHeatwaves.R
@@ -266,7 +266,7 @@ IDHeatwavesCPPwrapper <- function(datafr, threshold){
         tempsExceedingthreshold <- c(tempsExceedingthreshold, 0)
 
         # Identify heatwaves using the C++ functions.
-        heatwaves <- IDHeatwavesCPP(days, tempsExceedingthreshold)
+        heatwaves <- IDHeatwavesCPP(2, tempsExceedingthreshold)
 
         # Attach heatwaves columns onto the data in the datafr
         # variable. Note that the final row, which contains zeroes as

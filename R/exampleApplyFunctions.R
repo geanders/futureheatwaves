49e5b99fc28d3eaa405061de663fb78d30f5555d
Brooke Anderson <geanderson@jhsph.edu>
2016-02-19 15:20:21 -0700

    Close #85

--- R/exampleApplyFunctions.R
+++ R/exampleApplyFunctions.R
@@ -9,9 +9,9 @@
 #' dataframe with one and only one row per heatwave.
 #'
 #' @param hw_datafr A dataframe of heatwaves and their characteristics, as
-#'    created by \code{\link{gen_hw_set}}.
+#'    created by \code{\link{gen_hw_set}}
 #'
-#' @return A numeric value giving the number of heatwaves in the dataframe.
+#' @return A numeric value giving the number of heatwaves in the dataframe
 #'
 #' @export
 number_of_heatwaves <- function(hw_datafr){
@@ -19,18 +19,55 @@ number_of_heatwaves <- function(hw_datafr){
         return(out)
 }
 
+#' Calculate total heatwave days
+#'
+#' This function takes a dataframe of heatwaves, as created by
+#' \code{\link{gen_hw_set}}, and calculates the total number of heatwave days in
+#' the dataframe (sum of the number of days in each heatwave for all heatwaves
+#' in the dataset).
+#'
+#' @param hw_datafr A dataframe of heatwaves and their characteristics, as
+#'    created by \code{\link{gen_hw_set}}
+#'
+#' @return A numeric value giving the number of heatwave days in the dataframe
+#'
 #' @export
 heatwave_days <- function(hw_datafr){
         out <- sum(hw_datafr$length)
         return(out)
 }
 
+#' Calculate average length of heatwaves
+#'
+#' This function takes a dataframe of heatwaves, as created by
+#' \code{\link{gen_hw_set}}, and calculates the average length (in days) of
+#' heatwaves in the dataframe.
+#'
+#' @param hw_datafr A dataframe of heatwaves and their characteristics, as
+#'    created by \code{\link{gen_hw_set}}
+#'
+#' @return A numeric value giving the average length of heatwaves in the
+#'    dataframe
+#'
 #' @export
 average_length <- function(hw_datafr){
         out <- mean(hw_datafr$length)
         return(out)
 }
 
+#' Calculate average temperature of heatwaves
+#'
+#' This function takes a dataframe of heatwaves, as created by
+#' \code{\link{gen_hw_set}}, and calculates the average of the mean daily
+#' temperature during each heatwave across all heatwaves in the dataframe.
+#'
+#' @param hw_datafr A dataframe of heatwaves and their characteristics, as
+#'    created by \code{\link{gen_hw_set}}
+#'
+#' @return A numeric value giving the average of the mean daily
+#'    temperature during each heatwave across all heatwaves in the dataframe,
+#'    in degrees Fahrenheit
+#'
 #' @export
 average_mean_temp <- function(hw_datafr){
         out <- mean(hw_datafr$mean.temp)
--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 # .onLoad <- function(libname, pkgname){
 #         Rcpp::loadRcppModules()
 # }
-# .onUnLoad <- function(libpath){
+# .onUnload <- function(libpath){
 #         library.dynam.unload("futureheatwaves", libpath)
 # }

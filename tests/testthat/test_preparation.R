28965bcc06a209b8566d405648b0deb9cc34f923
Brooke Anderson <geanderson@jhsph.edu>
2016-02-19 09:37:26 -0700

    Work on tests

--- tests/testthat/test_preparation.R
+++ tests/testthat/test_preparation.R
@@ -20,31 +20,37 @@ finalList <- acquireDirectoryStructure(dataFolder = dataFolder,
                                                 "rcp85" = c(2006, 2099)),
                                        threshold_ensemble = "r1i1p1")
 
-# finalList_1 <- acquireDirectoryStructure(dataFolder = dataFolder,
-#                                        coordinateFilenames = coordinateFilenames,
-#                                        tasFilenames = tasFilenames,
-#                                        timeFilenames = timeFilenames,
-#                                        models_to_run = mods_1,
-#                                        dataDirectories = list(
-#                                                "historical" = c(1980, 2004),
-#                                                "rcp85" = c(2006, 2099)))
-#
-# finalList_2 <- acquireDirectoryStructure(dataFolder = dataFolder,
-#                                          coordinateFilenames = coordinateFilenames,
-#                                          tasFilenames = tasFilenames,
-#                                          timeFilenames = timeFilenames,
-#                                          models_to_run = mods_2,
-#                                          dataDirectories = list(
-#                                                  "historical" = c(1980, 2004),
-#                                                  "rcp85" = c(2006, 2099)))
-#
-# finalList_3 <- acquireDirectoryStructure(dataFolder = dataFolder,
-#                                          coordinateFilenames = coordinateFilenames,
-#                                          tasFilenames = tasFilenames,
-#                                          timeFilenames = timeFilenames,
-#                                          models_to_run = mods_3)
-#
-# test_that("User can specify which models to run", {
-#         expect_equal(sapply(finalList, FUN = function(x) x[[1]]), c("bcc1", "bcc1-a"))
-#         # expect_equal(sapply(finalList_1, FUN = function(x) x[[1]]), "bcc1")
-# })
+finalList_1 <- acquireDirectoryStructure(dataFolder = dataFolder,
+                                       coordinateFilenames = coordinateFilenames,
+                                       tasFilenames = tasFilenames,
+                                       timeFilenames = timeFilenames,
+                                       models_to_run = mods_1,
+                                       dataDirectories = list(
+                                               "historical" = c(1980, 2004),
+                                               "rcp85" = c(2006, 2099)),
+                                       threshold_ensemble = "r1i1p1")
+
+finalList_2 <- acquireDirectoryStructure(dataFolder = dataFolder,
+                                         coordinateFilenames = coordinateFilenames,
+                                         tasFilenames = tasFilenames,
+                                         timeFilenames = timeFilenames,
+                                         models_to_run = mods_2,
+                                         dataDirectories = list(
+                                                 "historical" = c(1980, 2004),
+                                                 "rcp85" = c(2006, 2099)),
+                                         threshold_ensemble = "r1i1p1")
+
+finalList_3 <- acquireDirectoryStructure(dataFolder = dataFolder,
+                                         coordinateFilenames = coordinateFilenames,
+                                         tasFilenames = tasFilenames,
+                                         timeFilenames = timeFilenames,
+                                         models_to_run = mods_3,
+                                         dataDirectories = list(
+                                                 "historical" = c(1980, 2004),
+                                                 "rcp85" = c(2006, 2099)),
+                                         threshold_ensemble = "r1i1p1")
+
+test_that("User can specify which models to run", {
+        expect_equal(sapply(finalList, FUN = function(x) x[[1]]), c("bcc1", "ccsm"))
+        # expect_equal(sapply(finalList_1, FUN = function(x) x[[1]]), "bcc1")
+})

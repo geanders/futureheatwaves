98d3e944ab417262fba77ee7733aaed142e71b23
Brooke Anderson <geanderson@jhsph.edu>
2016-02-19 10:08:31 -0700

    Work on vignette

--- inst/doc/futureheatwaves.R
+++ inst/doc/futureheatwaves.R
@@ -8,12 +8,25 @@ system.file("cities.csv", package = "futureheatwaves")
 system.file("cmip5", package = "futureheatwaves")
 
 ## ----eval = FALSE--------------------------------------------------------
+#  projection_dir_location <- system.file("cmip5", package = "futureheatwaves")
+#  city_file_location <- system.file("cities.csv", package = "futureheatwaves")
+#  
 #  gen_hw_set(out = "example_results",
-#             dataFolder = "cmip5" ,
-#             citycsv = "cities.csv",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1980, 2004),
+#                                          "rcp85" = c(2006, 2099)),
+#             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
-#             timeFilenames = "time_NorthAmerica_12mo.csv")
+#             timeFilenames = "time_NorthAmerica_12mo.csv",
+#             lat_lon_colnames = c("lat", "long"))
+
+## ------------------------------------------------------------------------
+data(hw_datafr)
+
+## ------------------------------------------------------------------------
+hw_datafr[1:3, c("hw.number", "mean.temp", "length", "start.date",
+                 "mean.temp.quantile", "city")]
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  # Define the function
@@ -42,39 +55,72 @@ system.file("cmip5", package = "futureheatwaves")
 out <- system.file(package = "futureheatwaves")
 map_grid(plot_model = "bcc1", out = out)
 
+## ----fig.width = 5, fig.height = 7---------------------------------------
+library(gridExtra)
+a <- map_grid(plot_model = "bcc1", out = out)
+b <- map_grid(plot_model = "ccsm", out = out)
+grid.arrange(a, b, ncol = 1)
+
+## ------------------------------------------------------------------------
+data(datafr)
+
+## ------------------------------------------------------------------------
+head(datafr, 3)
+id_of_hws <- IDHeatwavesR(datafr = datafr, threshold = 95)
+head(id_of_hws, 3)
+
 ## ---- eval = FALSE-------------------------------------------------------
+#  # Set up path names to example data
+#  projection_dir_location <- system.file("cmip5", package = "futureheatwaves")
+#  city_file_location <- system.file("cities.csv", package = "futureheatwaves")
+#  
 #  gen_hw_set(out = "example_results",
-#             dataFolder = "cmip5" ,
-#             citycsv = "cities.csv",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1980, 2004),
+#                                          "rcp85" = c(2006, 2099)),
+#             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
 #             timeFilenames = "time_NorthAmerica_12mo.csv",
+#             lat_lon_colnames = c("lat", "long"),
 #             IDheatwavesFunction = "IDHeatwavesAlternative")
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  gen_hw_set(out = "example_results",
-#             dataFolder = "cmip5" ,
-#             citycsv = "cities.csv",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1980, 2004),
+#                                          "rcp85" = c(2006, 2099)),
+#             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
 #             timeFilenames = "time_NorthAmerica_12mo.csv",
+#             lat_lon_colnames = c("lat", "long"),
+#             IDheatwavesFunction = "IDHeatwavesAlternative",
 #             projectionBoundaries = c(2071, 2090))
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  gen_hw_set(out = "example_results",
-#             dataFolder = "cmip5" ,
-#             citycsv = "cities.csv",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1980, 2004),
+#                                          "rcp85" = c(2006, 2099)),
+#             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
 #             timeFilenames = "time_NorthAmerica_12mo.csv",
+#             lat_lon_colnames = c("lat", "long"),
+#             IDheatwavesFunction = "IDHeatwavesAlternative",
 #             thresholdBoundaries = c(2061, 2080))
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  gen_hw_set(out = "example_results",
-#             dataFolder = "cmip5" ,
-#             citycsv = "cities.csv",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1980, 2004),
+#                                          "rcp85" = c(2006, 2099)),
+#             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
 #             timeFilenames = "time_NorthAmerica_12mo.csv",
-#             referenceBoundaries = c(2031, 2040))
+#             lat_lon_colnames = c("lat", "long"),
+#             IDheatwavesFunction = "IDHeatwavesAlternative",
+#             referencdeBoundaries = c(2031, 2040))
 

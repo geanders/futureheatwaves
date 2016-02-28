6222a7e6c5e80d6748cd5cc683aae68aa49e8824
Brooke Anderson <geanderson@jhsph.edu>
2016-02-28 15:15:49 -0700

    Fix test to allow `numDays`

--- tests/testthat/test_IDHeatwaves.R
+++ tests/testthat/test_IDHeatwaves.R
@@ -4,7 +4,7 @@ context("Identifying heatwaves")
 data("datafr", package = "futureheatwaves")
 hws_r <- IDHeatwavesR(datafr = datafr, threshold = 84.632, numDays = 2)
 hws_cpp <- IDHeatwavesCPPwrapper(datafr = datafr, threshold = 84.632,
-                                 numDays)
+                                 numDays = 2)
 
 test_that("C++ and R ID heatwaves identify same heatwaves", {
         expect_equal(hws_r$hw, hws_cpp$hw)

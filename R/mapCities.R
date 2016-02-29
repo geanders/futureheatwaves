6dc14b4c1b0b6013441961958dadf8179f381598
Brooke Anderson <geanderson@jhsph.edu>
2016-02-28 18:26:22 -0700

    Update documentation with new example file location

--- R/mapCities.R
+++ R/mapCities.R
@@ -11,7 +11,7 @@
 #'    edited using \code{ggplot2} functions.
 #'
 #' @examples
-#' out <- system.file(package = "futureheatwaves")
+#' out <- system.file("example_results", package = "futureheatwaves")
 #' map_grid(plot_model = "bcc1", out = out)
 #'
 #' @export

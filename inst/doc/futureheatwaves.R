37a6b2552e6215b9f88f1da2d3f70c06ddcdf893
Brooke Anderson <geanderson@jhsph.edu>
2016-03-23 21:04:46 -0600

    Final edits of vignette

--- inst/doc/futureheatwaves.R
+++ inst/doc/futureheatwaves.R
@@ -6,15 +6,17 @@ library(ggplot2)
 system.file("extdata/cities.csv", package = "futureheatwaves")
 
 ## ----eval = FALSE--------------------------------------------------------
+#  # Identify location of example files
 #  projection_dir_location <- system.file("extdata/cmip5",
 #                                         package = "futureheatwaves")
 #  city_file_location <- system.file("extdata/cities.csv",
 #                                    package = "futureheatwaves")
 #  
+#  # Process example files
 #  gen_hw_set(out = "example_results",
 #             dataFolder = projection_dir_location ,
 #             dataDirectories = list("historical" = c(1990, 1999),
-#                                         "rcp85" = c(2060, 2079)),
+#                                          "rcp85" = c(2060, 2079)),
 #             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
@@ -22,36 +24,64 @@ system.file("extdata/cities.csv", package = "futureheatwaves")
 
 ## ------------------------------------------------------------------------
 data(hw_datafr)
-
-## ------------------------------------------------------------------------
 hw_datafr[1:3, c("hw.number", "mean.temp", "length", "start.date",
                  "mean.temp.quantile", "city")]
 
 ## ------------------------------------------------------------------------
 system.file("extdata/cmip5", package = "futureheatwaves")
 
-## ---- eval = FALSE-------------------------------------------------------
-#  # Define the function
-#  average_mean_temp <- function(hw_datafr){
-#          out <- mean(hw_datafr$mean.temp)
-#          return(out)
-#          }
+## ------------------------------------------------------------------------
+average_mean_temp <- function(hw_datafr){
+        out <- mean(hw_datafr$mean.temp)
+        return(out)
+        }
 
 ## ------------------------------------------------------------------------
 out <- system.file("extdata/example_results", package = "futureheatwaves")
-apply_all_models(out = out, 
-                 FUN = average_mean_temp)
+apply_all_models(out = out, FUN = average_mean_temp)
 
 ## ------------------------------------------------------------------------
-apply_all_models(out = out, 
-                 FUN = average_mean_temp,
-                 city_specific = TRUE)
+apply_all_models(out = out, FUN = average_mean_temp, city_specific = TRUE)
 
-## ---- eval = FALSE-------------------------------------------------------
-#  average_mean_temp
+## ------------------------------------------------------------------------
+excess_deaths <- function(hw_datafr, base_mortality, RR = 1.032){
+        hw_datafr <- dplyr::left_join(hw_datafr, base_mortality,
+                                      by = "city") %>%
+                dplyr::mutate(excess_deaths = base_mort * length * RR)
+        out <- sum(hw_datafr$excess_deaths)
+        return(out)
+}
+
+## ----eval = FALSE--------------------------------------------------------
+#  apply_all_models(out = out, FUN = excess_deaths, base_mortality = base_mort)
+
+## ------------------------------------------------------------------------
+average_mean_temp
 
 ## ----eval = FALSE--------------------------------------------------------
 #  data(hw_datafr)
+
+## ----eval = FALSE--------------------------------------------------------
+#  gen_hw_set(out = "example_results",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1990, 1999),
+#                                          "rcp85" = c(2060, 2079)),
+#             citycsv = city_file_location,
+#             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
+#             tasFilenames = "tas_NorthAmerica_12mo.csv",
+#             timeFilenames = "time_NorthAmerica_12mo.csv",
+#             probThreshold = 0.99)
+
+## ----eval = FALSE--------------------------------------------------------
+#  gen_hw_set(out = "example_results",
+#             dataFolder = projection_dir_location ,
+#             dataDirectories = list("historical" = c(1990, 1999),
+#                                          "rcp85" = c(2060, 2079)),
+#             citycsv = city_file_location,
+#             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
+#             tasFilenames = "tas_NorthAmerica_12mo.csv",
+#             timeFilenames = "time_NorthAmerica_12mo.csv",
+#             thresholdBoundaries = c(2070, 2079))
 
 ## ------------------------------------------------------------------------
 data(datafr)
@@ -62,12 +92,6 @@ id_of_hws <- IDHeatwavesR(datafr = datafr, threshold = 95, numDays = 2)
 head(id_of_hws, 3)
 
 ## ---- eval = FALSE-------------------------------------------------------
-#  # Set up path names to example data
-#  projection_dir_location <- system.file("extdata/cmip5",
-#                                         package = "futureheatwaves")
-#  city_file_location <- system.file("extdata/cities.csv",
-#                                    package = "futureheatwaves")
-#  
 #  gen_hw_set(out = "example_results",
 #             dataFolder = projection_dir_location ,
 #             dataDirectories = list("historical" = c(1990, 1999),
@@ -82,18 +106,18 @@ head(id_of_hws, 3)
 #  gen_hw_set(out = "example_results",
 #             dataFolder = projection_dir_location ,
 #             dataDirectories = list("historical" = c(1990, 1999),
-#                                    "rcp85" = c(2060, 2079)),
+#                                          "rcp85" = c(2060, 2079)),
 #             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
 #             timeFilenames = "time_NorthAmerica_12mo.csv",
-#             projectionBoundaries = c(2060, 2069))
+#             projectionBoundaries = c(2060, 2079))
 
 ## ---- eval = FALSE-------------------------------------------------------
 #  gen_hw_set(out = "example_results",
 #             dataFolder = projection_dir_location ,
 #             dataDirectories = list("historical" = c(1990, 1999),
-#                                         "rcp85" = c(2060, 2079)),
+#                                          "rcp85" = c(2060, 2079)),
 #             citycsv = city_file_location,
 #             coordinateFilenames = "latitude_longitude_NorthAmerica_12mo.csv",
 #             tasFilenames = "tas_NorthAmerica_12mo.csv",
